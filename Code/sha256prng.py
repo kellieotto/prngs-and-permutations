47e16459e2e1f56ffb3d74b188ec964987eb711a
Kellie Ottoboni <kellieotto@berkeley.edu>
2016-09-29 10:39:33 -0700

    Break prng into two classes: a base rng class and a sha256 specific one

--- Code/sha256prng.py
+++ Code/sha256prng.py
@@ -10,7 +10,11 @@ RECIP_BPF = 2**-BPF
 HASHLEN = 256 # Number of bits in a hash output
 RECIP_HASHLEN = 2**-HASHLEN
 
-class MyRandom(random.Random):
+################################################################################
+############################## Base PRNG Class #################################
+################################################################################
+
+class BaseRandom(random.Random):
     '''Random number generator base class'''
 
     def __init__(self, x=None):
@@ -20,7 +24,6 @@ class MyRandom(random.Random):
         """
 
         self.seed(x)
-        self.next = None
 
 
     def seed(self, a=None, counter=0):
@@ -35,57 +38,61 @@ class MyRandom(random.Random):
         the prng gets called.
         """
         # TODO: how to seed, see Ron's implementation
-        self.seedv = a
-        self.count = counter
+        self.seed = a
+        self.counter = counter
+
+        
+    def next(self):
+        """
+        Update the counter
+        """
+        self.counter += 1
+        
+    
+    def getstate(self):
+        return (self.seed, self.counter)
+        
+    
+    def setstate(self, state):
+        """
+        Set the state (seed and counter)
+        """
+        (self.seed, self.counter) = (int(val) for val in state)
+    
+    
+    def jumpahead(self, n):
+        """
+        Jump ahead n steps in the period
+        """
+        self.counter += n        
+
+
+################################################################################
+############################## SHA-256 Class ###################################
+################################################################################
+
+class SHA256(BaseRandom):
+    """
+    PRNG based on the SHA-256 cryptographic hash function.
+    """
+    
+#    def __init__(self): # do a args/kwargs thing to set seed
+#        return None
         
     
     def random(self):
-        """Get the next random number in the range [0.0, 1.0)."""
-        hash_input = (str(self.seedv) + "," + str(self.count)).encode('utf-8')
+        hash_input = (str(self.seed) + "," + str(self.counter)).encode('utf-8')
         # Apply SHA-256, interpreting hex output as hexadecimal integer
         # to yield 256-bit integer (a python "long integer")
         hash_output = int(hashlib.sha256(hash_input).hexdigest(),16)
+        self.next()
         return(hash_output*RECIP_HASHLEN)
-
-
-	def getstate():
-        """Return internal state; can be passed to setstate() later."""
-        return self.VERSION, super(Random, self).getstate(), self.gauss_next
-	    # TODO: does this need to be changed?
         
-    def setstate(self, state):
-        """Restore internal state from object returned by getstate()."""
-        version = state[0]
-        if version == 3:
-            version, internalstate, self.gauss_next = state
-            super(Random, self).setstate(internalstate)
-        elif version == 2:
-            version, internalstate, self.gauss_next = state
-            # In version 2, the state was saved as signed ints, which causes
-            #   inconsistencies between 32/64-bit systems. The state is
-            #   really unsigned 32-bit ints, so we convert negative ints from
-            #   version 2 to positive longs for version 3.
-            try:
-                internalstate = tuple( long(x) % (2**32) for x in internalstate )
-            except ValueError, e:
-                raise TypeError, e
-            super(Random, self).setstate(internalstate)
-        else:
-            raise ValueError("state with version %s passed to "
-                             "Random.setstate() of version %s" %
-                             (version, self.VERSION))
-
-    def jumpahead(self, n):
-        """Change the internal state to one that is n steps ahead of the
-        current state.
+        
+#    def __repr__(self):
         """
-        # The super.jumpahead() method uses shuffling to change state,
-        # so it needs a large and "interesting" n to work with.  Here,
-        # we use hashing to create a large n for the shuffle.
-        s = repr(self.getstate()) + repr(n)
-        self.setstate(state=s) # might need to use super(Random, self).setstate instead
-        
-        
+        what does this do?
+        """
         
 ################################################################################
 ############################## some sample code ################################
@@ -102,3 +109,17 @@ hash_output = int(hashlib.sha256(hash_input).hexdigest(),16)
 
 print(hash_output*RECIP_HASHLEN)
 count += 1
+
+
+# using the class above
+r = BaseRandom(5)
+r.getstate()
+# (5, 0)
+r.next()
+r.getstate()
+# (5, 1)
+r.jumpahead(5)
+r.getstate()
+# (5, 6)
+r.random()
+r.randint(5, 10)
