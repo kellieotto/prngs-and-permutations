00a7352fb300db3c55ca168806807f2dbc699719
Kellie Ottoboni <kellieotto@berkeley.edu>
2016-10-23 11:41:12 -0700

    Add sims for RANDU with an actual random seed

--- Code/statBiasSimulations/pyscripts/RANDU.py
+++ Code/statBiasSimulations/pyscripts/RANDU.py
@@ -8,23 +8,25 @@ import csv
 reps = int(5 * 10**7) 
 n = [30]
 k = [2]
-ss = 100
+s = [100, 233424280]
 
 for nn in n:
     for kk in k:
         if kk >= nn:
             continue
+            
+        for ss in s:
         
-        lcg = lcgRandom(seed=ss) # set seed of RANDU to 100
+            lcg = lcgRandom(seed=ss) # set seed of RANDU
 
-        uniqueSampleCounts = getEmpiricalDistr(lcg, n=nn, k=kk, reps=reps)
-        with open('../rawdata/US_RANDU.csv', 'at') as csv_file:
-            writer = csv.writer(csv_file)
-            for key, value in uniqueSampleCounts.items():
-                writer.writerow([key, value, nn, kk, ss])
+            uniqueSampleCounts = getEmpiricalDistr(lcg, n=nn, k=kk, reps=reps)
+            with open('../rawdata/US_RANDU.csv', 'at') as csv_file:
+                writer = csv.writer(csv_file)
+                for key, value in uniqueSampleCounts.items():
+                    writer.writerow([key, value, nn, kk, ss])
         
-        itemCounts = getItemCounts(uniqueSampleCounts)    
-        with open('../rawdata/FO_RANDU.csv', 'at') as csv_file:
-            writer = csv.writer(csv_file)
-            for key, value in itemCounts.items():
-                writer.writerow([key, value, nn, kk, ss])
+            itemCounts = getItemCounts(uniqueSampleCounts)    
+            with open('../rawdata/FO_RANDU.csv', 'at') as csv_file:
+                writer = csv.writer(csv_file)
+                for key, value in itemCounts.items():
+                    writer.writerow([key, value, nn, kk, ss])
