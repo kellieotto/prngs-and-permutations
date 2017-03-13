529d801ee85b1ed1387f7235090f770fb7799442
Kellie Ottoboni <kellieotto@berkeley.edu>
2017-03-13 16:19:40 -0700

    Update derangements SPRT to track both alternatives

--- Code/SPRT/pyscripts/derangements_5.py
+++ Code/SPRT/pyscripts/derangements_5.py
@@ -1,6 +1,6 @@
 ################################################################################
 # SLURM Array Job 5
-# SPRT of permutation derangements, SHA256, n=10, permute_indices
+# SPRT of permutation derangements, SHA256, n=100, permute_indices
 ################################################################################
 
 import numpy as np
@@ -15,7 +15,8 @@ from sha256prng import SHA256
 
 np.random.seed(347728688) # From random.org Timestamp: 2017-01-19 18:22:16 UTC
 seed_values = np.random.randint(low = 1, high = 2**32, size = 1000)
-column_names = ["prng", "algorithm", "seed", "decision", "LR", "pvalue", "steps"]
+column_names = ["prng", "algorithm", "seed", "decision_upper", "LR_upper", "steps_upper",
+                 "decision_lower", "LR_lower", "steps_lower"]
 
 ################################################################################
 # SPRT functions
@@ -42,61 +43,95 @@ def sequential_derangement_test(sampling_function, n, alpha, beta, multiplier, m
     '''
     Conduct Wald's SPRT for whether derangements occur more or less frequently than 1/e
     H_0: derangements occur with equal frequency (p approx 1/e)
-    H_1: p = p1 = multiplier * p0, multiplier in the range (0, 1/p0)
+    H_1 upper: p = p1 = multiplier * p0 > p0
+    H_1 lower: p = p1_lower = (2-multiplier) * p0 < p0
     
     sampling_function: a function which generates a random permutation
     n: number of items
     alpha: desired type 1 error rate
     beta: desired type 2 error rate
-    multiplier: value in (0, 1/p0). Determines alternative hypothesis
-    maxsteps: maximum number of trials before stopping the test. Default is 10**8.
+    multiplier: value in (1, 1/p0). Determines the "greater than" alternative hypothesis,
+        and 2-multiplier determines the "less than" alternative hypothesis
+    maxsteps: maximum number of trials before stopping the test. Default is 10**7.
     '''
 
+    assert multiplier > 1
     assert maxsteps > 0
 
     # Set p0 = probability of a derangement
     p0 = prob_derangement(n)
     p1 = multiplier*p0
+    p1_lower = (2-multiplier)*p0
+    assert p1 < 1
+    assert p1_lower < 1
+    assert p0 < 1
     
     # Set parameters
     lower = beta/(1-alpha)
     upper = (1-beta)/alpha
-    lr_occurs = p1/p0
-    lr_doesnotoccur = (1 - p1)/(1 - p0)
-
-    LR = [1]
-    decision = 'None'        
+    lr_occurs_upper = p1/p0
+    lr_doesnotoccur_upper = (1 - p1)/(1 - p0)
+    lr_occurs_lower = p1_lower/p0
+    lr_doesnotoccur_lower = (1 - p1_lower)/(1 - p0)
+    
+    LR_upper = [1]
+    LR_lower = [1]
+    decision_upper = None        
+    decision_lower = None        
     vec = np.array(range(0, n))
     steps = 0
     
+    lower_test_unfinished = 1
+    upper_test_unfinished = 1
+    
     # Draw samples
-    while lower < LR[-1] < upper and steps < maxsteps:
+    while steps < maxsteps and (lower_test_unfinished + upper_test_unfinished):
         steps += 1
         perm = sampling_function(n)
         Dn = check_derangement(vec, perm)
 
-        if Dn:
-            LR.append(LR[-1] * lr_occurs)
-        else:
-            LR.append(LR[-1] * lr_doesnotoccur)
+        # Run test at step n for greater than alternative
+        if upper_test_unfinished:     
+            if Dn:
+                LR_upper.append(LR_upper[-1] * lr_occurs_upper)
+            else:
+                LR_upper.append(LR_upper[-1] * lr_doesnotoccur_upper)
             
-        # Run test at step n
-        if LR[-1] <= lower:
-            # accept the null and stop
-            decision = 0
-            break
-
-        if LR[-1] >= upper:
-            # reject the null and stop
-            decision = 1
-            break
+            if LR_upper[-1] <= lower:
+                # accept the null and stop
+                decision_upper = 0
+                upper_test_unfinished = 0
+                
+            if LR_upper[-1] >= upper:
+                # reject the null and stop
+                decision_upper = 1
+                upper_test_unfinished = 0 
             
-    return {'decision' : decision,
-            'lower' : lower,
-            'LR' : LR,
-            'upper' : upper,
-            'steps' : steps,
-            'pvalue' : min(1/LR[-1], 1)
+        # Run test at step n for less than alternative
+        if lower_test_unfinished:     
+            if Dn:
+                LR_lower.append(LR_lower[-1] * lr_occurs_lower)
+            else:
+                LR_lower.append(LR_lower[-1] * lr_doesnotoccur_lower)
+            
+            if LR_lower[-1] <= lower:
+                # accept the null and stop
+                decision_lower = 0
+                lower_test_unfinished = 0
+                
+            if LR_lower[-1] >= upper:
+                # reject the null and stop
+                decision_lower = 1
+                lower_test_unfinished = 0       
+                
+    return {'decision_upper' : decision_upper,
+            'decision_lower' : decision_lower,
+            'lower_threshold' : lower,
+            'upper_threshold' : upper,
+            'LR_upper' : LR_upper,
+            'LR_lower' : LR_lower,
+            'steps_lower' : len(LR_lower)-1,
+            'steps_upper' : len(LR_upper)-1
             }
             
             
@@ -109,9 +144,9 @@ def testSeed(ss):
     prng = SHA256(ss)
 
     sampling_func = lambda n: permute_indices(n, prng)
-    res = sequential_derangement_test(sampling_func, n=10, alpha=0.05, beta=0, multiplier=1.1)
-    return ["SHA256", "permute_indices", ss, res['decision'], res['LR'][-1], res['pvalue'], res['steps']]
-    
+    res = sequential_derangement_test(sampling_func, n=100, alpha=0.05/2, beta=0, multiplier=1.1)
+    return ["SHA256", "permute_indices", res['decision_upper'], res['LR_upper'][-1], res['steps_upper'],
+            res['decision_lower'], res['LR_lower'][-1], res['steps_lower']]    
     
 
 def wrapper(i):
@@ -157,7 +192,7 @@ result = lview.map(testSeed, seed_values)
 
 # Write results to file
 
-with open('../rawdata/SHA256_derangements_pi_n10.csv', 'at') as csv_file:
+with open('../rawdata/SHA256_derangements_pi_n100.csv', 'at') as csv_file:
 	writer = csv.writer(csv_file)
 	writer.writerow(column_names)
 	for i in range(len(result)):
